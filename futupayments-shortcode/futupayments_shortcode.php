8168ac84aa2e4aba9d3b8eaa4f05c21dffb655b7
Maxim Oransky <maxim.oransky@gmail.com>
2014-06-23 21:15:09 +0400

    callbacks

--- futupayments-shortcode/futupayments_shortcode.php
+++ futupayments-shortcode/futupayments_shortcode.php
@@ -12,28 +12,58 @@ if (!class_exists('FutubankForm')) {
 }
 
 class _FutupaymentsShortcode {
-    const DB_VERSION = '1.0';
+    const DB_VERSION     = '2';
 
     const SETTINGS_GROUP = 'futupayments-shortcode-optionz';
-    const SETTINGS_SLUG = 'futupayments-shortcode';
+    const SETTINGS_SLUG  = 'futupayments-shortcode';
     
-    const SUCCESS_URL = '/?futupayment-success';
-    const FAIL_URL    = '/?futupayment-fail';
-    const SUBMIT_URL  = '/?futupayment-submit';
+    const SUCCESS_URL    = '/?futupayment-success';
+    const FAIL_URL       = '/?futupayment-fail';
+    const SUBMIT_URL     = '/?futupayment-submit';
+    const CALLBACK_URL   = '/?futupayment-callback';
+    
+    const STATUS_UNKNOWN = 'unknown';
+    const STATUS_PAID    = 'paid';
+    const STATUS_ERROR   = 'error';
 
+    private $order_table;
+    private $order_table_format;
     private $templates_dir;
-    private $db_prefix;
+    private $invoice_hidden_fields;
+    private $change_status = true;
 
     function __construct() {
         global $wpdb;
-        $this->db_prefix = $wpdb->prefix . 'futupayments_';    
+        
+        $db_prefix = $wpdb->prefix . 'futupayments_';    
+        $this->order_table = $db_prefix . 'order';
+        
+        $this->order_table_format = array(
+            '%s',  # creation_datetime
+            '%s',  # amount
+            '%s',  # currency 
+            '%s',  # description 
+            '%s',  # client_email 
+            '%s',  # client_name 
+            '%s',  # client_phone 
+            '%s',  # status
+            '%s',  # meta
+        );
+        
+        $this->invoice_hidden_fields =array(
+            'amount',
+            'currency',
+            'description',
+            'cancel_url',
+        );
+
         $this->templates_dir = dirname(__FILE__) . '/templates/';
 
         add_action('init',  array($this, 'init'));
         add_shortcode('futupayment', array($this, 'futupayment'));
         if (is_admin()) {
             add_action('admin_menu', array($this, 'admin_menu'));
-            add_action('plugins_loaded',  array($this, 'plugins_loaded'));
+            add_action('plugins_loaded', array($this, 'plugins_loaded'));
             add_action('admin_init', array($this, 'admin_init'));
         }
         add_action('parse_request', array($this, 'parse_request'));
@@ -48,9 +78,10 @@ class _FutupaymentsShortcode {
 
     function parse_request() {
         foreach (array(
-            self::SUCCESS_URL => array($this, 'success_page'),
-            self::FAIL_URL    => array($this, 'fail_page'),
-            self::SUBMIT_URL  => array($this, 'submit_page'),
+            self::SUCCESS_URL  => array($this, 'success_page'),
+            self::FAIL_URL     => array($this, 'fail_page'),
+            self::SUBMIT_URL   => array($this, 'submit_page'),
+            self::CALLBACK_URL => array($this, 'callback_page'),
         ) as $url => $func) {
             if ($_SERVER['REQUEST_URI'] == $url) {
                 call_user_func($func);
@@ -59,27 +90,11 @@ class _FutupaymentsShortcode {
         }
     }
 
-    function myplugin_update_db_check() {
-        global $jal_db_version;
-        if (get_site_option( 'jal_db_version' ) != $jal_db_version) {
-            jal_install();
-        }
-    }
-    
-    private function get_invoice_hidden_fields() {
-        return array(
-            'amount',
-            'currency',
-            'description',
-            'page_url',
-        );
-    }
-
     function futupayment($atts) {
         $ff = $this->get_futubank_form();
 
         if (!$ff) {
-            return __('FUTUPAYMENT ERROR: plugin is not configured', 'futupayments');
+            return __('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('plugin is not configured', 'futupayments');
         }
 
         $atts = shortcode_atts(array(
@@ -89,21 +104,20 @@ class _FutupaymentsShortcode {
         ), $atts);
 
         if (!$atts['amount']) {
-            return __('FUTUPAYMENT ERROR: amount required', 'futupayments');
+            return __('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('amount required', 'futupayments');
         }
 
         if (!$atts['currency']) {
-            return __('FUTUPAYMENT ERROR: currency required', 'futupayments');
+            return __('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('currency required', 'futupayments');
         }
 
         if (!$atts['description']) {
-            return __('FUTUPAYMENT ERROR: description required', 'futupayments');
+            return __('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('description required', 'futupayments');
         }
 
-        $atts['page_url'] = get_home_url() . $_SERVER['REQUEST_URI'];
-        
+        $atts['cancel_url'] = get_home_url() . $_SERVER['REQUEST_URI'];
         $h = array();
-        foreach ($this->get_invoice_hidden_fields() as $k) {
+        foreach ($this->invoice_hidden_fields as $k) {
             $h[$k] = $atts[$k];
         }
         $atts['signature'] = $ff->get_signature($h);
@@ -114,7 +128,8 @@ class _FutupaymentsShortcode {
             '<form action="' . self::SUBMIT_URL . '" method="post">' .
                 FutubankForm::array_to_hidden_fields($atts) .
                 '<p class="submit">' .
-                    '<input type="submit" name="submit" class="button button-primary" value="' . esc_attr($options['pay_button_text']) . '">' .
+                    '<input type="submit" name="submit" class="button button-primary"' .
+                    ' value="' . esc_attr($options['pay_button_text']) . '">' .
                 '</p>' .
             '</form>'
         );
@@ -145,13 +160,14 @@ class _FutupaymentsShortcode {
 
     function list_page() { 
         global $wpdb;
-        $rows = $wpdb->get_results('SELECT * FROM ' . $this->db_prefix . 'order ORDER BY id DESC', ARRAY_A);
+        $rows = $wpdb->get_results('SELECT * FROM ' . $this->order_table . ' ORDER BY id DESC', ARRAY_A);
         include $this->templates_dir . 'list.php';
     }
 
     function settings_page() {
         $group = self::SETTINGS_GROUP;
         $slug = self::SETTINGS_SLUG;
+        $callback_url = get_home_url() . self::CALLBACK_URL;
         include $this->templates_dir . 'settings.php';
     }
 
@@ -232,8 +248,15 @@ class _FutupaymentsShortcode {
 
     private function get_futubank_form() {
         $options = $this->get_options();
-        if ($options['merchant_id'] && $options['secret_key']) {
-            return new FutubankForm($options['merchant_id'], $options['secret_key'], $options['test_mode']);
+        if (
+            $options['merchant_id'] && 
+            $options['secret_key']
+        ) {
+            return new FutubankForm(
+                $options['merchant_id'], 
+                $options['secret_key'], 
+                $options['test_mode']
+            );
         } else {
             return false;
         }
@@ -257,42 +280,53 @@ class _FutupaymentsShortcode {
         return $result;
     }
 
-    private function create_order(array $h) {
+    private function create_order(array $data) {
+        global $wpdb;
         $order = array(
-            'amount'            => $h['amount'],
-            'currency'          => $h['currency'],
-            'description'       => $h['description'],
-            'client_email'      => self::get($h, 'client_email', ''),
-            'client_name'       => self::get($h, 'client_name', ''),
-            'client_phone'      => self::get($h, 'client_phone', ''),
             'creation_datetime' => current_time('mysql'),
+            'amount'            => $data['amount'],
+            'currency'          => $data['currency'],
+            'description'       => $data['description'],
+            'client_email'      => self::get($data, 'client_email', ''),
+            'client_name'       => self::get($data, 'client_name', ''),
+            'client_phone'      => self::get($data, 'client_phone', ''),
+            'status'            => self::STATUS_UNKNOWN,
+            'meta'              => '',
         );
 
-        global $wpdb;
-        $wpdb->insert($this->db_prefix . 'order', $order, array(
-            '%s',
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s',
-        )) or die(__('FUTUPAYMENT ERROR: can\'t create order', 'futupayments'));
+        $wpdb->insert($this->order_table, $order) or die(__('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('can\'t create order', 'futupayments'));
         $order['id'] = $wpdb->insert_id;
-
         return $order;
+    }
+    
+    private function load_order($order_id) {
+        global $wpdb;
+        return $wpdb->get_row('SELECT * FROM ' . $this->order_table . ' WHERE id = ' . $order_id, ARRAY_A);
+    }
+    
+    private function save_order(adday $order) {
+        global $wpdb;
+        $order_id = array_pop($order, 'id');
+        return (bool) $wpdb->update( 
+            'table',
+            $order,
+            array('id' => $order_id),
+            $this->order_table_format,
+            array('%d')
+        );
     }
 
     private function submit_page() {
-        $ff = $this->get_futubank_form() or die(__('FUTUPAYMENT ERROR: plugin is not configured', 'futupayments'));
+        $ff = $this->get_futubank_form() or 
+            die(__('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('plugin is not configured', 'futupayments'));
         
         $h = array();
-        foreach ($this->get_invoice_hidden_fields() as $k) {
+        foreach ($this->invoice_hidden_fields() as $k) {
             $h[$k] = self::get($_POST, $k);
         }
 
         if ($ff->get_signature($h) != self::get($_POST, 'signature')) {
-            die(__('FUTUPAYMENT ERROR: incorrect data', 'futupayments'));
+            die(__('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('incorrect data', 'futupayments'));
         }
 
         $options = $this->get_options();
@@ -308,7 +342,7 @@ class _FutupaymentsShortcode {
             $order['client_phone'],
             $options['success_url'],
             $options['fail_url'],
-            $_POST['page_url'],
+            $_POST['cancel_url'],
             $meta,
             $order['description']
         );
@@ -317,34 +351,82 @@ class _FutupaymentsShortcode {
     }
 
     private function success_page() {
-        $ff = $this->get_futubank_form() or die(__('FUTUPAYMENT ERROR: plugin is not configured', 'futupayments'));
-        echo 'success!';
+        $ff = $this->get_futubank_form() or 
+            die(__('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('plugin is not configured', 'futupayments'));
+        include $this->templates_dir . 'success.php';
     }
 
     private function fail_page() {
-        $ff = $this->get_futubank_form() or die(__('FUTUPAYMENT ERROR: plugin is not configured', 'futupayments'));
-        echo 'fail!';
+        $ff = $this->get_futubank_form() or 
+        die(__('FUTUPAYMENT ERROR', 'futupayments') . ': ' . __('plugin is not configured', 'futupayments'));
+        include $this->templates_dir . 'fail.php';
+    }
+
+    private function callback_page() {
+        $ff = $this->get_futubank_form();
+
+        $error = null;
+        if (!$ff->is_signature_correct($_POST)) {
+            $error = 'Incorrect "signature"';
+        } else if (!($order_id = (int) self::get($_POST, 'order_id', 0))) {
+            $error = 'Empty "order_id"';
+        } else if (!($order = $this->load_order($order_id))) {
+            $error = 'Unknown order_id';
+        } else if ($order['currency'] != self::get($_POST, 'currency')) {
+            $error = "Currency mismatch: '$order[currency]'' != '$_POST[currency]'";
+        } else if ($order['amount'] != self::get($_POST, 'amount')) {
+            $error = "Amount mismatch: '$order[amount]'' != '$_POST[amount]'";
+        }
+
+        if ($error) {
+            echo "ERROR: $error\n";
+        } else {
+            echo "OK$order_id\n";
+            if ($ff->is_order_completed($_POST)) {
+                echo "order completed\n";
+                if ($this->change_status) {
+                    if ($order['status'] == self::STATUS_PAID) {
+                        echo "already paid\n";
+                    } else {
+                        $order['status'] = self::STATUS_PAID;
+                        $order['meta'] = self::get($_POST, 'meta', '');
+                        if ($this->save_order($order)) {
+                            echo "paid now\n";
+                        } else {
+                            echo "ERROR: can't change payment status\n";
+                        }
+                    }
+                }
+            } else {
+                echo "order not completed\n";
+                if ($order['status'] != self::STATUS_PAID) {
+                    $order['status'] = self::STATUS_ERROR;
+                    $order['meta'] = self::get($_POST, 'meta', '');
+                    $this->save_order($order);
+                }
+            }
+        }
     }
 
     private function create_plugin_tables() {
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        // dbDelta("
+        //     CREATE TABLE `" . $this->db_prefix . "payment` (
+        //         `id` integer AUTO_INCREMENT NOT NULL,
+        //         `creation_datetime` datetime NOT NULL,
+        //         `transaction_id` bigint NOT NULL,
+        //         `testing` bool NOT NULL,
+        //         `amount` numeric(10, 2) NOT NULL,
+        //         `currency` varchar(3) NOT NULL,
+        //         `order_id` varchar(128) NOT NULL,
+        //         `state` varchar(10) NOT NULL,
+        //         `message` longtext NOT NULL,
+        //         `meta` longtext NOT NULL,
+        //         UNIQUE (`state`, `transaction_id`)
+        //     );
+        // ");
         dbDelta("
-            CREATE TABLE `" . $this->db_prefix . "payment` (
-                `id` integer AUTO_INCREMENT NOT NULL,
-                `creation_datetime` datetime NOT NULL,
-                `transaction_id` bigint NOT NULL,
-                `testing` bool NOT NULL,
-                `amount` numeric(10, 2) NOT NULL,
-                `currency` varchar(3) NOT NULL,
-                `order_id` varchar(128) NOT NULL,
-                `state` varchar(10) NOT NULL,
-                `message` longtext NOT NULL,
-                `meta` longtext NOT NULL,
-                UNIQUE (`state`, `transaction_id`)
-            );
-        ");
-        dbDelta("
-            CREATE TABLE `" . $this->db_prefix . "order` (
+            CREATE TABLE `" . $this->order_table . "` (
                 `id` integer AUTO_INCREMENT NOT NULL,
                 `creation_datetime` datetime NOT NULL,
                 `amount` numeric(10, 2) NOT NULL,
@@ -352,13 +434,14 @@ class _FutupaymentsShortcode {
                 `description` longtext NOT NULL,
                 `client_email` varchar(120) NOT NULL,
                 `client_name` varchar(120) NOT NULL,
-                `client_phone` varchar(30) NOT NULL
+                `client_phone` varchar(30) NOT NULL,
+                `status` varchar(30) NOT NULL default '" . self::STATUS_UNKNOWN . "',
+                `meta` longtext NOT NULL
             );
         ");
     }
 
     ## helpers ##
-
     private static function get(array $hash, $key, $default=null) {
         if (array_key_exists($key, $hash)) {
             return $hash[$key];
--- futupayments-shortcode/templates/settings.php
+++ futupayments-shortcode/templates/settings.php
@@ -1,6 +1,9 @@
 <form action="options.php" method="post">
     <?php settings_fields($group); ?>
     <?php do_settings_sections($slug); ?>
+    <p>
+    	<strong><?php _e('Callback URL'); ?>:</strong> <?php echo $callback_url; ?>
+    </p>
     <p class="submit">
         <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes'); ?>">
     </p>
